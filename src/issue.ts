cdf0a8f7d709ea6e60290d2b0f0cf2aac9e7d639
Tobias Wochinger <t.wochinger@rasa.com>
2020-04-24 12:15:26 +0200

    check if issue with this content already exists

--- src/issue.ts
+++ src/issue.ts
@@ -1,11 +1,26 @@
 import { Octokit } from '@octokit/rest';
+import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { IssueOption, IssueResponse } from './interface';
 
-export async function createIssue(
+async function get_trivy_issues(repository: string, token: string) {
+  const client: Octokit = new Octokit({ auth: token });
+
+  let trivyIssues = await client.issues.list(
+      { state: "open", labels: "tool:trivy" , filter: "all" })
+
+  trivyIssues = trivyIssues.data.filter(issue => issue.repository.full_name == repository)
+
+  core.info(`Found ${trivyIssues.length} open trivy issues for this repository.`)
+  core.exportVariable('issues', trivyIssues);
+
+  return trivyIssues
+}
+
+async function createIssue(
   token: string,
   options: IssueOption
-): Promise<IssueResponse> {
+): Promise<any> {
   const client: Octokit = new Octokit({ auth: token });
   const {
     data: issue,
@@ -15,9 +30,33 @@ export async function createIssue(
       ...options,
     }
   );
+  return issue
+}
+
+export async function getExistingOrCreateIssue(
+  token: string,
+  options: IssueOption
+): Promise<IssueResponse> {
+  const currentVulnerability = options.body.match(/\|CVE-[0-9-]+\|/)
+  core.info(`Current vulnerability is ${currentVulnerability}`)
+  core.exportVariable('v', currentVulnerability);
+
+  let issue = null
+  if (currentVulnerability != null) {
+    const trivyIssues = await get_trivy_issues(github.context.repo, token)
+    issue = trivyIssues.find(issue => issue.body.includes(currentVulnerability[0]))
+  }
+
+  if (issue != null) {
+    core.info("Found existing issue. Skip creating a new one.")
+  } else {
+    issue = await createIssue(token, options)
+  }
+
   const result: IssueResponse = {
     issueNumber: issue.number,
     htmlUrl: issue.html_url,
   };
+
   return result;
 }
